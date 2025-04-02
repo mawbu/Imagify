831243aa2af26fc2cd02049d93380abbfce9aaf4
mawbu <109028440+mawbu@users.noreply.github.com>
2025-04-02 21:27:49 +0700

    Update vite.config.js

--- Desktop/Imagify/client/vite.config.js
+++ Desktop/Imagify/client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',          // Cho phép kết nối từ bên ngoài (Render)
+    port: process.env.PORT || 5173  // Dùng cổng do Render cung cấp
+  }
 })
