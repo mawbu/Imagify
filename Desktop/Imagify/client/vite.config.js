f1c163945c6a700ac5218b9525a713f668f90109
mawbu <109028440+mawbu@users.noreply.github.com>
2025-04-02 21:31:23 +0700

    Update vite.config.js

--- Desktop/Imagify/client/vite.config.js
+++ Desktop/Imagify/client/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',          // Cho phép kết nối từ bên ngoài (Render)
-    port: process.env.PORT || 5173  // Dùng cổng do Render cung cấp
+    host: '0.0.0.0',          
+    port: Number(process.env.PORT) || 5173,  
+    strictPort: true,  
+    allowedHosts: ['imagify-mabuw.onrender.com'],  // Cho phép domain của Render
   }
 })
