c543edf0725130b1754de06890162f13ff4af42d
mabuw <nguyenhuycanh2505@gmail.com>
2025-04-02 19:57:51 +0700

    Complete payment

--- Desktop/Imagify/client/src/context/Appcontext.jsx
+++ Desktop/Imagify/client/src/context/Appcontext.jsx
@@ -1,16 +1,84 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [credit, setCredit] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const navigate = useNavigate();
+
+  const loadCreditsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/credits", {
+        headers: { token },
+      });
+      if (data.success) {
+        setCredit(data.credits);
+        setUser(data.user);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const generateImage = async (prompt) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/image/generate-image",
+        { prompt },
+        {
+          headers: { token },
+        }
+      );
+      if (data.success) {
+        loadCreditsData();
+        return data.resultImage;
+      } else {
+        toast.error(data.message);
+        loadCreditsData();
+        if (data.creditBalance <= 0) {
+          navigate("/buy");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setUser(null);
+  };
+
+  useEffect(() => {
+    if (token) {
+      loadCreditsData();
+    }
+  }, [token]);
 
   const value = {
     user,
     setUser,
     showLogin,
     setShowLogin,
+    token,
+    setToken,
+    credit,
+    setCredit,
+    backendUrl,
+    loadCreditsData,
+    logout,
+    generateImage,
   };
 
   return (
